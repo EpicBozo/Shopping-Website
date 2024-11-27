aaa5941fd2b41a98ccb3e764a6f28a8dfb5534f1
EpicBozo <aymanahsan06@gmail.com>
2024-11-26 21:28:58 -0500

    scroll through the whole page to load all of the html elements, kinda didnt work tho

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 # Create your views here.
 
@@ -22,8 +23,22 @@ def scraper(product_id):
     url = "https://www.aliexpress.us/w/wholesale-" + product_id + ".html"
     driver.get(url)
 
-   
+    #Gets height of the entire page
+    page_height = driver.execute_script("return document.body.scrollHeight")
 
+    while True:
+        # Scroll to the bottom of the page
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        time.sleep(5) # Loads the whole page
+
+        current_height = driver.execute_script("return document.body.scrollHeight")
+
+        if page_height == current_height:
+            break
+
+        page_height = current_height
+    
 def results(request):
     text = request.POST.get('search')
     scraper(text)
