babeec8aa83476637691e173be2f71968d84b97f
Ayman Ahsan <aymanahsan06@gmail.com>
2024-12-02 18:54:05 -0500

    fix

--- myproject/myapp/static/js/script.js
+++ myproject/myapp/static/js/script.js
@@ -1,7 +1,6 @@
-const categories_button = document.getElementById("dropdown_button") 
+const categories_button = document.getElementById("dropdown_button");
 const dropdownContent = document.querySelector(".dropdown_content")
 
-if(categories_button && dropdownContent){
 categories_button.addEventListener("click", function(){
     if (categories_button.classList.contains("off")){
         categories_button.classList.remove("off");
@@ -16,6 +15,4 @@ categories_button.addEventListener("click", function(){
     } else {
         dropdownContent.style.display = "none";
     }
-    
 });
-}
