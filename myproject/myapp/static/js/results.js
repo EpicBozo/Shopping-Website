8acc5b9632d7879f751e85a0709eb28bba39256f
Ayman Ahsan <aymanahsan06@gmail.com>
2024-12-03 17:48:18 -0500

    added card view handeling

--- myproject/myapp/static/js/results.js
+++ myproject/myapp/static/js/results.js
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', function (event) {
     var high_to_low_button = document.getElementById("high-to-low");
     var sort_by_input = document.getElementById("sort-by");
     var sort_form = document.getElementById("sort-by-form");
+    var product = document.querySelectorAll(".product");
+    var grid_view = document.getElementById("grid");
+    var list_view = document.getElementById("list");
     //Filter dropdown handeling
 
         filter_button.addEventListener("click", function () {
@@ -38,4 +41,30 @@ document.addEventListener('DOMContentLoaded', function (event) {
             sort_by_input.value = "high-to-low";
             sort_form.submit();
             });
+
+        //Handeling the product cards
+        grid_view.addEventListener("click", function () {
+            if(grid_view.classList.contains("off")){
+                grid_view.classList.remove("off");
+                grid_view.classList.add("on");
+                list_view.classList.remove("on");
+                list_view.classList.add("off");
+                for (var i = 0; i < product.length; i++) {
+                    product[i].style.width = "15%";
+                }
+                   
+            }
+        });
+
+        list_view.addEventListener("click", function () {
+            if(list_view.classList.contains("off")){
+                list_view.classList.remove("off");
+                list_view.classList.add("on");
+                grid_view.classList.remove("on");
+                grid_view.classList.add("off");
+                for (var i = 0; i < product.length; i++) {
+                    product[i].style.width = "80%";
+                }
+            }
+        });
 });
